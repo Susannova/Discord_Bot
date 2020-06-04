7d35f0e254c915fcdcab2a28cb4ccd2b878c9615
Jan Büddefeld <susannova@web.de>
2020-06-04 12:15:44 +0200

    Documentation

--- core/state.py
+++ core/state.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class GuildState:
     """State class for one guild that saves all variables that need to have a
     global state at runtime that potentially has to change during
-    runtime. 
+    runtime.
     """
     def __init__(self):
         self.debug = False
@@ -31,10 +31,12 @@ class GeneralState:
         self.guilds_state = {}
 
     def get_version(self):
+        """ Returns the git master head """
         version_file = open("./.git/refs/heads/master", "r")
         return version_file.read()[:7]
 
     def write_state_to_file(self):
+        """ Pickles the state to a file """
         filename = f'{self.config.general_config.database_directory_global_state}/{self.config.general_config.database_name_global_state}'
         try:
             with open(filename, 'wb') as file:
@@ -47,12 +49,14 @@ class GeneralState:
             logger.error('Global state was not pickable. Content was written to %s', filename_failed)
     
     def add_guild_state(self, guild_id: int):
+        """ Adds the guild state. Raises a KeyError if guild already exists """
         if guild_id in self.guilds_state:
             raise KeyError(f"Can't add {guild_id} because guild already exists.")
         else:
             self.guilds_state[guild_id] = GuildState()
 
     def get_guild_state(self, guild_id: int) -> GuildState:
+        """ Returns the guild state. Raises a KeyError if guild does not exist """
         if guild_id not in self.guilds_state:
             raise KeyError(f"{guild_id} does not exists!.")
         else:
