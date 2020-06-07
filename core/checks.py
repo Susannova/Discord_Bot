024185221bca99f8ce150c3ba39284de67a19760
Jan Büddefeld <susannova@web.de>
2020-06-07 13:10:44 +0200

    Rewrite "is_in_channels" and "has_any_role"

--- core/checks.py
+++ core/checks.py
@@ -13,55 +13,53 @@ from . import (
 
 
 def has_any_role(*role_names):
-    """ Checks if the author of the context has any of the roles or is the owner of the guild """
-    def _has_any_role(func):
-        async def wrapper(obj, ctx, *args):
-            roles_to_check = obj.bot.config.get_guild_config(ctx.guild.id).get_role_ids(*role_names).values()
+    async def wrapper(ctx: commands.Context):
+        bot = ctx.bot
+        roles_to_check = bot.config.get_guild_config(ctx.guild.id).get_role_ids(*role_names).values()
+
+        role_ids_author = [role.id for role in ctx.message.author.roles]
         
-            role_ids_author = [role.id for role in ctx.message.author.roles]
-            
-            # if list is empty the condition is false
-            if ctx.guild.owner == ctx.author or [role_id for role_id in role_ids_author if role_id in roles_to_check]:
-                return await func(obj, ctx, *args)
-            else:
-                raise commands.MissingAnyRole(roles_to_check)
-        return wrapper
-    return _has_any_role
+        # if list is empty the condition is false
+        if ctx.guild.owner == ctx.author or [role_id for role_id in role_ids_author if role_id in roles_to_check]:
+            return True
+        else:
+            raise commands.MissingAnyRole(roles_to_check)
+    
+    return commands.check(wrapper)
 
 
 def is_in_channels(*channel_names):
-    """ Decorator to check if ctx is in one of the channels. If not an exception is raised, otherwise the decorated function is executed.
-    No exception will be raised and the function will always be executed if the ctx is in a bot channel. """
-    def _is_in_channels(func):
-        async def wrapper(obj, ctx: commands.Context, *args):
-            bot = obj.bot
-            guild_config = bot.config.get_guild_config(ctx.guild.id)
+    """ Decorator to check if ctx is in one of the channels """
+    async def wrapper(ctx: commands.Context):
+        bot = ctx.bot
 
-            channels_dict = dataclasses.asdict(guild_config.channel_ids)
-            
-            # TODO Can be None!!
-            category_temp = bot.get_channel(channels_dict["category_temporary"])
-            
-            channels_dict["temporary_channels"] = []
-            
-            if category_temp is not None:
-                for temp_channel in category_temp.channels:
-                    channels_dict["temporary_channels"].append(temp_channel.id)
-            
-            channels_to_check = []
-            for channel_name in channel_names:
-                if channel_name in channels_dict:
-                    channels_to_check += channels_dict[channel_name]
-            channels_to_check += guild_config.get_category_ids(channel_names)
-            channels_to_check += channels_dict["bot"]
-            
-            # if list is empty the condition is false
-            if ctx.message.channel.id in channels_to_check:
-                return await func(obj, ctx, *args)
-            else:
-                raise commands.CheckFailure("Command is used in false channel")
-        return wrapper
-    return _is_in_channels
+        guild_config = bot.config.get_guild_config(ctx.guild.id)
+
+        channels_dict = dataclasses.asdict(guild_config.channel_ids)
+        
+        # TODO Can be None!!
+        category_temp = bot.get_channel(channels_dict["category_temporary"])
+        
+        channels_dict["temporary_channels"] = []
+        
+        if category_temp is not None:
+            for temp_channel in category_temp.channels:
+                channels_dict["temporary_channels"].append(temp_channel.id)
+        
+        channels_to_check = []
+        for channel_name in channel_names:
+            if channel_name in channels_dict:
+                channels_to_check += channels_dict[channel_name]
+        channels_to_check += guild_config.get_category_ids(channel_names)
+        channels_to_check += channels_dict["bot"]
+        
+        # if list is empty the condition is false
+        if ctx.message.channel.id in channels_to_check:
+            return True
+        else:
+            raise commands.CheckFailure("Command is used in false channel")
+    return commands.check(wrapper)
+
 
 def has_n_attachments(n):
     async def predicate(ctx):
