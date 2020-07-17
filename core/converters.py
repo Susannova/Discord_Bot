0855c399566d7d76a80898ffcddbb5efd005d454
Jan Büddefeld <susannova@web.de>
2020-07-17 13:30:01 +0200

    ArgsToDict supports lists now! Code is really ugly..

--- core/converters.py
+++ core/converters.py
@@ -2,17 +2,19 @@ import re
 from discord.ext import commands
 
 class ArgsToDict(commands.Converter):
-	async def convert(self, ctx: commands.Context, args: str) -> dict:
-		""" Converts args to a dict.
-		Important: Spaces in args will be ignored!
+    async def convert(self, ctx: commands.Context, args: str) -> dict:
+        """ Converts args to a dict.
+        Important: Spaces in args will be ignored!
 
-		Format of args has to be keys and a value for each key. The assignment is done with a ':' or a '='
-		the key values pairs are seperated by ',' or ';'.
-		"""
+        Format of args has to be keys and a value for each key. The assignment is done with a ':' or a '='
+        the key values pairs are seperated by ',' or ';'.
+        """
 
-		# This is a generator that yields generators that yield at first a key and then a value as a string.
-		dict_list = (
-			(key_value for key_value in re.split("=|:", arg)) for arg in re.split(",|;", args.replace(" ", ""))
-		)
+        args_formatted = args.replace(" ", "").replace("=", ":").replace("(", "[").replace(";", ",").replace(")", "]")
 
-		return dict(dict_list)
+        # This is a generator that yields generators that yield at first a key and then a value. The value is either a string or a list. Short but really ugly..
+        dict_list = (
+            (key_val_str if re.fullmatch("[\[].*[\]]", key_val_str) is None else [item for item in re.findall("[^,\[\]]+", key_val_str)] for key_val_str in arg.split(":")) for arg in re.findall("[^,]:[\[].*?[\]]|[^,]:[^\[\],]", args_formatted)
+        )
+
+        return dict(dict_list)
