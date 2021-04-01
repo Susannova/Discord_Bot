97546906bd673ebe9cd7ab84a84a57cb95faf399
Nick W <nick.wittig@outlook.com>
2021-04-01 13:26:22 +0200

    Refactored play() method
    Extracted some methods out of the play() method to make it easier to maintain.
    Deleted some old/unnecssary code.
    Imported typing classes instead of the whole module.
    Reformatted some docstings.

--- cogs/cog_play_requests.py
+++ cogs/cog_play_requests.py
@@ -3,7 +3,7 @@ import re
 import logging
 import asyncio
 import datetime
-import typing
+from typing import List, Optional
 
 import discord
 from discord.ext import commands
@@ -17,6 +17,7 @@ from core import (
     converters,
     config
 )
+from core.config import Game
 
 from core.play_requests import PlayRequest
 
@@ -25,18 +26,18 @@ logger = logging.getLogger(__name__)
 
 class PlayRequestsCog(commands.Cog, name='Play-Request Commands'):
     def __init__(self, bot: DiscordBot.KrautBot):
-        
         self.bot = bot
     
     async def cog_check(self, ctx: commands.Context):
         return await checks.command_is_allowed(ctx)
 
     @commands.group()
-    async def play(self, ctx: commands.Context, games: commands.Greedy[converters.StrToGame], play_time: typing.Optional[converters.StrToTime], player_needed_num: typing.Optional[int], should_be_empty: typing.Optional[str]):
-        """ Create a play request
+    async def play(self, ctx: commands.Context, games: commands.Greedy[converters.StrToGame], play_time: Optional[converters.StrToTime], player_needed_num: Optional[int], should_be_empty: Optional[str]):
+        """ 
+        Creates a play request.
 
         Other players can react to the play request which will notify the other players.
-        5 Minutes before the play time, all players will be notified too.
+        Five Minutes before the play time, all players will be notified too.
 
         Args:
             games: The games to play
@@ -44,80 +45,86 @@ class PlayRequestsCog(commands.Cog, name='Play-Request Commands'):
             player_needed_num: The amount of players still needed to fill the play-request.
             should_be_empty: A string to check if everything worked. Leave this always empty please.
         """
-        if ctx.invoked_subcommand is None:
-            logger.info('Create a play request')
-            guild_config = self.bot.config.get_guild_config(ctx.guild.id)
+        logger.info('Trying to create a play request')
+        if ctx.invoked_subcommand is not None:
+            return
 
-            if should_be_empty is not None:
-                logger.warning("Something went wrong. Was able to interpret the play request until: %s.", should_be_empty)
-                await ctx.send(f"Something went wrong. I was able to interpret the command until '{should_be_empty}'. Try `{self.bot.get_command_prefix(ctx.guild.id)}help play`.")
-                raise ValueError("Was not able to interpret the command")
+        if should_be_empty is not None:
+            logger.warning("Something went wrong. Was able to interpret the play request until: %s.", should_be_empty)
+            await ctx.send(f"Something went wrong. I was able to interpret the command until '{should_be_empty}'. Try `{self.bot.get_command_prefix(ctx.guild.id)}help play`.")
+            raise ValueError("Was not able to interpret the command")
 
-            games_temp = []
-            for game in games:
-                if game not in games_temp:
-                    games_temp.append(game)
-            games = games_temp
+        guild_config = self.bot.config.get_guild_config(ctx.guild.id)
 
-            if not games:
-                logger.debug("Play request created without a game. Create list of games based on author")
-                user_role_ids = [role.id for role in ctx.author.roles]
-                games = [game for game in guild_config.yield_all_games() if game.role_id in user_role_ids]
-            
-            is_now = True if play_time is None else False
+        if not games:
+            logger.debug("Play request created without a game. Create list of games based on author")
+            games = self.__get_assinged_games(ctx, guild_config)
+            if len(games) == 0:
+                raise ValueError("There were no games given in the play request.")
+                await ctx.send("There were no games given in the play request.")
 
-            if not is_now:
-                if (play_time - datetime.datetime.now()).total_seconds() / 60 > guild_config.unsorted_config.play_now_time_add_limit:
-                    logger.warning("Play request denied because of time")
-                    await ctx.send(f"Play requests that are going more than {guild_config.unsorted_config.play_now_time_add_limit} minutes in the future are not allowed.")
-                    return
-            else:
-                play_time = datetime.datetime.now()
+        is_now = True if play_time is None else False
 
-            message_unformated = guild_config.messages.play_now if is_now else guild_config.messages.play_at
-  
-            
-            game_names = games[0].name_long
-            if len(games) > 1:
-                # TODO Make this more general!
-                for game in games[1:-1]:
-                    game_names += f", {game.name_long}"
-                game_names += f" oder  {games[-1].name_long}"
-            
-            date_str = ""
-            if not play_time.date() == datetime.datetime.now().date():
-                date = guild_config.messages.date_format.format(
-                    day=play_time.strftime("%d"),
-                    month=play_time.strftime("%m")
-                )
-                date_str = guild_config.messages.play_at_date.format(date=date)
+        if not is_now:
+            if (play_time - datetime.datetime.now()).total_seconds() / 60 > guild_config.unsorted_config.play_now_time_add_limit:
+                logger.warning("Play request denied because of time")
+                await ctx.send(f"Play requests that are going more than {guild_config.unsorted_config.play_now_time_add_limit} minutes in the future are not allowed.")
+                return
+        else:
+            play_time = datetime.datetime.now()
 
-            message = message_unformated.format(
-                    role_mention=" ".join((ctx.guild.get_role(game.role_id).mention for game in games)),
-                    creator=ctx.message.author.mention,
-                    player=ctx.message.author.mention,
-                    game=game_names,
-                    time=play_time.strftime("%H:%M"),
-                    date_str=date_str
-                )
-            if isinstance(player_needed_num, int):
-                if player_needed_num > 0 and player_needed_num < 20:
-                    message = f'{message} {guild_config.messages.players_needed.format(player_needed_num=player_needed_num)}'
+        message_unformated = guild_config.messages.play_now if is_now else guild_config.messages.play_at
+        
+        game_names = self.__get_game_name_message(games)
+        
+        date_str = ""
+        if not play_time.date() == datetime.datetime.now().date():
+            date = guild_config.messages.date_format.format(
+                day=play_time.strftime("%d"),
+                month=play_time.strftime("%m")
+            )
+            date_str = guild_config.messages.play_at_date.format(date=date)
 
-            play_request_channel = self.bot.get_channel(guild_config.channel_ids.play_request)
-            play_request_message = await play_request_channel.send(message, delete_after=guild_config.unsorted_config.auto_delete_after_seconds)
+        message = message_unformated.format(
+                role_mention=" ".join((ctx.guild.get_role(game.role_id).mention for game in games)),
+                creator=ctx.message.author.mention,
+                player=ctx.message.author.mention,
+                game=game_names,
+                time=play_time.strftime("%H:%M"),
+                date_str=date_str
+            )
+        if isinstance(player_needed_num, int):
+            if player_needed_num > 0 and player_needed_num < 20:
+                message = f'{message} {guild_config.messages.players_needed.format(player_needed_num=player_needed_num)}'
 
-            _category = [game.name_short for game in games]
-            play_request = PlayRequest(play_request_message.id, ctx.message.author.id, category=_category, play_time=play_time)
+        play_request_channel = self.bot.get_channel(guild_config.channel_ids.play_request)
+        play_request_message = await play_request_channel.send(message, delete_after=guild_config.unsorted_config.auto_delete_after_seconds)
 
-            self.bot.state.get_guild_state(ctx.guild.id).add_play_request(play_request)
-            await self.add_auto_reaction(play_request_message, games)
+        _category = [game.name_short for game in games]
+        play_request = PlayRequest(play_request_message.id, ctx.message.author.id, category=_category, play_time=play_time)
 
-            await ctx.message.delete(delay=3)
+        self.bot.state.get_guild_state(ctx.guild.id).add_play_request(play_request)
+        await self.add_auto_reaction(play_request_message, games)
+        await ctx.message.delete(delay=3)
 
-            if not is_now:
-                await play_request.auto_reminder(guild_config=guild_config, bot=self.bot)
-    
+        if not is_now:
+            await play_request.auto_reminder(guild_config=guild_config, bot=self.bot)
+
+    def __get_assinged_games(self, ctx: commands.Context, guild_config) -> List[Game]:
+        """Gets all games that are assinged to the command creator."""
+        user_role_ids = [role.id for role in ctx.author.roles]
+        games = [game for game in guild_config.yield_all_games() if game.role_id in user_role_ids]
+        return games
+
+    def __get_game_name_message(self, games: List[Game]) -> List[str]:
+        """Gets the formatted game name string needed for the play-request message."""
+        game_names = games[0].name_long
+        if len(games) > 1:
+            for game in games[1:-1]:
+                game_names += f", {game.name_long}"
+            game_names += f" oder  {games[-1].name_long}"
+        return game_names
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if isinstance(message.channel, discord.DMChannel):
@@ -129,6 +136,7 @@ class PlayRequestsCog(commands.Cog, name='Play-Request Commands'):
         if message.channel.id == guild_config.channel_ids.play_request:
             if message.author == self.bot.user:
                 seconds_slept = 0
+                # FIXME: Do we need this? Where does sending message even get changed?
                 while self.bot.sending_message:
                     await asyncio.sleep(1)
                     seconds_slept += 1
@@ -146,7 +154,7 @@ class PlayRequestsCog(commands.Cog, name='Play-Request Commands'):
                 await message.delete(delay=delay)
 
 
-    async def add_auto_reaction(self, play_request_message: discord.Message, games: typing.List[config.Game]):
+    async def add_auto_reaction(self, play_request_message: discord.Message, games: List[config.Game]):
         for game in games:
             await play_request_message.add_reaction(self.bot.get_emoji(game.emoji))
 
