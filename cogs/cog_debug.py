173d268e84fc25dc3ab9a75a1b8ff1a7ef849f9a
thyanin <8885680+thyanin@users.noreply.github.com>
2021-04-03 19:09:31 +0200

    Hide clash_dates command and added Docstrings/Function Annotations

--- cogs/cog_debug.py
+++ cogs/cog_debug.py
@@ -1,5 +1,5 @@
 import logging
-import typing
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -17,13 +17,14 @@ class DebugCog(commands.Cog):
         return await checks.command_in_bot_channel_and_used_by_admin(ctx)
 
     @commands.command(name="version")
-    async def version_(self, ctx):
+    async def version_(self, ctx: commands.Context):
+        """Get the current bot version as a commit hash."""
         logger.debug("!version called")
         await ctx.send(self.bot.state.version)
 
     @commands.command(name="reload-ext")
     @commands.check(checks.is_super_user)
-    async def reload_ext_(self, ctx, ext):
+    async def reload_ext_(self, ctx: commands.Context, ext: str):
         logger.info("!reload-ext called")
         try:
             self.bot.reload_extension(ext)
@@ -44,14 +45,15 @@ class DebugCog(commands.Cog):
         await ctx.send(log_str)
 
     @commands.command(name="status")
-    async def status_(self, ctx):
+    async def status_(self, ctx: commands.Context):
+        """Check if the bot is alive."""
         logger.debug("!status called")
         await ctx.send("Bot is alive.")
 
     @commands.command(name="get_emoji_id")
-    async def get_emoji_id_(self, ctx, emoji_name):
+    async def get_emoji_id_(self, ctx: commands.Context, emoji_name: str):
+        """Get the ID of the given Emoji name(`str`)."""
         logger.debug("!get_emoji_id called")
-
         for emoji in ctx.guild.emojis:
             if emoji.name == emoji_name:
                 await ctx.send(f"Emoji id is {emoji.id}.")
@@ -75,7 +77,7 @@ class DebugCog(commands.Cog):
 
     @commands.command(name="print")
     @commands.check(checks.is_super_user)
-    async def print_(self, ctx, arg):
+    async def print_(self, ctx: commands.Context, arg: str):
         logger.debug("!print %s called", arg)
         # return_string = ast.literal_eval(arg)
         # less safe but more powerful
@@ -85,11 +87,12 @@ class DebugCog(commands.Cog):
 
     @commands.command(name="end")
     @commands.check(checks.is_super_user)
-    async def end_(self, ctx, *arg):
-        if len(list(arg)) == 0:
+    async def end_(self, ctx: commands.Context, arg: Optional[str] = None):
+        """Stops the bot."""
+        if arg is None:
             await ctx.send("Goodbye!")
             exit_status = 0
-        elif str(arg[0]) == "abort":
+        elif arg == "abort":
             await ctx.send("Bot will be restarted if systemd is configured to restart on failure.")
             exit_status = 3
         else:
@@ -100,12 +103,12 @@ class DebugCog(commands.Cog):
         await self.bot.logout(exit_status)
 
     @commands.command(name="dict")
-    async def dict_(self, ctx, *, dictonary: converters.ArgsToDict):
+    async def dict_(self, ctx: commands.Context, *, dictonary: converters.ArgsToDict):
         await ctx.send(str(dictonary))
 
     @commands.command()
     async def send_to_channel(
-        self, ctx: commands.Context, text_channel: discord.TextChannel, pin: typing.Optional[bool] = True, *, text: str
+        self, ctx: commands.Context, text_channel: discord.TextChannel, pin: Optional[bool] = True, *, text: str
     ):
         """Send a message to a text channel and pins it per default."""
         logger.info("Send a text to channel %s: %s", text_channel.name, text)
